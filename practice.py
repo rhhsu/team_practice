c135d343e8c0d6e3030edb5630ba176964a1a7b9
Saige Sunier <ses123@duke.edu>
2019-11-20 12:36:08 -0500

    goodbye

--- practice.py
+++ practice.py
@@ -2,5 +2,9 @@ def say_hello():
     print("What's up everyone thanks for coming out we're Bob Eats the Drum Kit and this is Rubbish Durham Gerbils.")
 
 
+def say_goodbye():
+    print("Thank you all for coming to the show, but sadly Bob Eats the Drum Kit must go!")
+
 if __name__ == "__main__":
     say_hello()
+    say_goodbye()
