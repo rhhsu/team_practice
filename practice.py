39820c3315ef3519f435537776eb24e838705ffe
Rhianne <rh208@duke.edu>
2019-11-20 12:32:52 -0500

    Initial commit.

---
+++ practice.py
@@ -0,0 +1,6 @@
+def say_hello():
+    print("What's up everyone thanks for coming out we're Bob Eats the Drum Kit and this is Rubbish Durham Gerbils.")
+
+
+if __name__ == "__main__":
+    say_hello()
