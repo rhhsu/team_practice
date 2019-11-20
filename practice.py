111d60746d3d08320badbf362aad1b0b423cb696
Aliza Kajani <aliza.kajani@gmail.com>
2019-11-20 12:34:51 -0500

    added function sing()

--- practice.py
+++ practice.py
@@ -2,5 +2,10 @@ def say_hello():
     print("What's up everyone thanks for coming out we're Bob Eats the Drum Kit and this is Rubbish Durham Gerbils.")
 
 
+def sing():
+    print("Rubbish. Durham. Gerbils. *guitar solo*")
+
+
 if __name__ == "__main__":
     say_hello()
+    sing()
